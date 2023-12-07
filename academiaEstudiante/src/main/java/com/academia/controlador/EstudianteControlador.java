2483b7f5899de556ed5f4ca5131d999cdd588cb7
Solano Calderon Jose Pablo <jsolanoca@ice.go.cr>
2023-12-07 15:33:52 -0600

    correccion

--- academiaEstudiante/src/main/java/com/academia/controlador/EstudianteControlador.java
+++ academiaEstudiante/src/main/java/com/academia/controlador/EstudianteControlador.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -64,5 +65,24 @@ public class EstudianteControlador {
 		}
 	}
 	
+	@PutMapping("/estudiante/{id}")
+	public ResponseEntity<?> actualizarEstudiante(@RequestBody Estudiante datosEstudiante, @PathVariable Integer id){
+		try {
+			Estudiante estudianteExiste = servicio.obtenerEstudianteById(id);
+			if (estudianteExiste != null) {
+				datosEstudiante.setId(id);
+				Estudiante estudianteGuardado = servicio.guardarEstudiante(datosEstudiante);
+				return new ResponseEntity<>(estudianteGuardado, HttpStatus.OK);
+				}else {
+					return new ResponseEntity<>("Estudiante no existe " + id, HttpStatus.NOT_FOUND);
+				}
+		} catch (Exception e) {
+			return new ResponseEntity<>("Error al actualizar " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		
+	}
+	
+
 	
 }
--- academiaEstudiante/src/main/java/com/academia/servicio/EstudianteServicio.java
+++ academiaEstudiante/src/main/java/com/academia/servicio/EstudianteServicio.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
-
 import com.academia.modelo.Estudiante;
 import com.academia.repositorio.EstudianteRepositorio;
 
@@ -24,9 +23,10 @@ public class EstudianteServicio {
 		return repositorio.findById(id).get();	
 	}
 	
-	public void guardarEstudiante(Estudiante datosEstudiante) {
-		repositorio.save(datosEstudiante);
+	public Estudiante guardarEstudiante(Estudiante datosEstudiante) {
+		return repositorio.save(datosEstudiante);
 	}
+
 
 	public List<Estudiante> obtenerEstudianteByCedula(String cedula){
 		return repositorio.findByCedulaEquals(cedula);
